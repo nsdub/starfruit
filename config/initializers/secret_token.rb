4a48b6f6f53a3c16a93851168351b24baf4f4ade
nsdub <nathanielsokollward@gmail.com>
2014-02-22 17:15:19 -0800

    first starfruit commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starfruit::Application.config.secret_key_base = '91f0ff5daaaa4e8c58b22ab3251c22de9ac4799d672e2917eefa73a14f20fa91721bec44fadabf8d99efc7290ae0606782217a8a6d381b1a06fa48fbdbb64066'
