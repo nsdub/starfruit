a938e25dd58e42f7a24ee95beb98bf36791fa332
nsdub <nathanielsokollward@gmail.com>
2014-03-02 18:10:11 -0800

    signout works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
-  include Pundit
-  protect_from_forgery
+  # include Pundit
+  # protect_from_forgery
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :role
+    registration_params = [:email, :password, :password_confirmation, :username, :role]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
   end
 end
